1aba65ee5a79da1233cdf4a87fc17db3f8cfecdc
Farzana-Bashar <zerin2645@gmail.com>
2021-09-06 23:49:04 +0600

    add: update serializer

--- api/views.py
+++ api/views.py
@@ -1,24 +1,44 @@
 from django.shortcuts import render
 import io
-from rest_framework.parsers import JsonParser
+from rest_framework.parsers import JSONParser
 from .models import Student
 from .serializers import StudentSerializer
 from rest_framework.renderers import JSONRenderer
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
+
+@csrf_exempt
 def student_api(request):
     if request.method == 'GET':
         json_data = request.body
         stream = io.BytesIO(json_data)
-        pythondata = JsonParser().parse(stream)
+        pythondata = JSONParser().parse(stream)
         id = pythondata.get('id', None)
+        # id = request.body.id
         if id is not None:
-            stu = Student.object.get(id = id)
+            stu = Student.objects.get(id = id)
             serializer = StudentSerializer(stu)
             json_data = JSONRenderer().render(serializer.data)
             return HttpResponse(json_data, content_type = 'application/json')
         
-        stu = Student.object.all()
+        stu = Student.objects.all()
         serializer = StudentSerializer(stu, many = True)
         json_data = JSONRenderer().render(serializer.data)
         return HttpResponse(json_data, content_type = 'application/json')
+    
+    if request.method == 'POST':
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        pythondata = JSONParser().parse(stream)
+        serializer = StudentSerializer(data = pythondata)
+
+        if serializer.is_valid():
+            serializer.save()
+            res = {'msg': 'data created'}
+            json_data = JSONRenderer().render(res)
+            return HttpResponse(json_data, content_type = 'application/json')
+        else:
+            json_data = JSONRenderer().render(serializer.errors)
+            return HttpResponse(json_data, content_type = 'application/json')
+
--- crud/myapp.py
+++ crud/myapp.py
@@ -8,10 +8,26 @@ def get_data(id = None):
     if id is not None:
         data = {'id': id}
     
-    json_data = json.dump(data)
+    json_data = json.dumps(data)
 
     r = requests.get(url = URL, data =json_data)
     data = r.json()
     print(data)
 
-get_data()
+# get_data(2)
+
+
+def post_data(id = None):
+    data = {
+        'name': 'shajal',
+        'roll': 31,
+        'city': 'magura'
+    }
+    
+    json_data = json.dumps(data)
+
+    r = requests.post(url = URL, data =json_data)
+    data = r.json()
+    print(data)
+
+post_data()
