e5a34a014d73674dc9ecc2e83d35bddfb1b43dd7
Farzana-Bashar <zerin2645@gmail.com>
2021-09-06 22:19:12 +0600

    add: views.py

--- api/views.py
+++ api/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+import io
+from rest_framework.parsers import JsonParser
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework.renderers import JSONRenderer
+from django.http import HttpResponse
 
-
+def student_api(request):
+    if request.method == 'GET':
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        pythondata = JsonParser().parse(stream)
+        id = pythondata.get('id', None)
+        if id is not None:
+            stu = Student.object.get(id = id)
+            serializer = StudentSerializer(stu)
+            json_data = JSONRenderer().render(serializer.data)
+            return HttpResponse(json_data, content_type = 'application/json')
